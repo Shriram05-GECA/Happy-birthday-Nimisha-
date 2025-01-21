c2b21bbd2461441b3467661ee284d51c37d1c920
Shriram Vaidya <shriramvaidya05@gmail.com>
2025-01-21 17:44:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('surpriseButton');
+const message = document.getElementById('message');
+
+button.addEventListener('click', () => {
+  message.style.display = 'block'; // Show the message
+});
